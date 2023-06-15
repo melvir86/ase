982885c252b80d26838662269a3bdd0ef0c13414
Dalird <dbufi01@student.bbk.ac.uk>
2023-06-15 19:17:01 +0200

    Adding code for application map phase one

---
+++ application/flaskr/cars.py
@@ -0,0 +1,45 @@
+from typing import Optional
+
+from sqlmodel import Field, SQLModel, Session, create_engine, select
+
+import decimal
+from decimal import Decimal
+
+class Car(SQLModel, table=True):
+  #id: Optional[int] = Field(default=None, primary_key=True)
+  vehicleId: str = Field(default=None, primary_key=True)
+  latitude: Decimal
+  longitude: Decimal
+  place: str
+
+car_1 = Car(vehicleId = 'mXfkjrFw', latitude = '51.5090562', longitude = '-0.1304571', place = '??? (A surprise)')
+car_2 = Car(vehicleId = 'nZXB8ZHz', latitude = '51.5080898', longitude = '-0.07620836346036469', place = 'Tower of London')
+car_3 = Car(vehicleId = 'Tkwu74WC', latitude = '51.5425649', longitude = '-0.00693080308689057', place = 'Westfield Stratford City')
+car_4 = Car(vehicleId = '5KWpnAJN', latitude = '51.519821199999996', longitude = '-0.09397523701275332', place = 'The Barbican Centre')
+car_5 = Car(vehicleId = 'uf5ZrXYw', latitude = '51.5133798', longitude = '-0.0889552', place = 'The Bank of England')
+car_6 = Car(vehicleId = 'VMerzMH8', latitude = '51.5253378', longitude = '-0.033435', place = 'Mile End Station')
+car_7 = Car(vehicleId = '8efT67Xd', latitude = '51.54458615', longitude = '-0.0161905117168855', place = 'Queen Elizabeth Olympic Park')
+
+print(car_1)
+print(car_2)
+
+engine = create_engine("sqlite:///database.db")
+
+SQLModel.metadata.create_all(engine)
+
+with Session(engine) as session:
+  session.add(car_1)
+  session.add(car_2)
+  session.add(car_3)
+  session.add(car_4)
+  session.add(car_5)
+  session.add(car_6)
+  session.add(car_7)
+  session.commit()
+  session.close()
+
+with Session(engine) as sess:
+  statement = select(Car).where(Car.vehicleId == 'mXfkjrFw')
+  result = sess.exec(statement).first()
+  print(result)
+  sess.close()
---
+++ application/flaskr/main.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template
+import folium
+from sqlmodel import Session, select, create_engine
+import sqlmodel
+from cars import Car
+
+app = Flask(__name__)
+
+@app.route('/')
+def fetch_map():
+    # Create a map object using Folium
+    map = folium.Map(location=[51.5074, -0.1278], zoom_start=12)  # London coordinates
+
+    # Create the engine
+    engine = create_engine("sqlite:///database.db")
+
+    # Fetch car data from the SQLite database
+    with Session(engine) as sess:
+        statement = select(Car)
+        results = sess.exec(statement).all()
+
+    # Png path of the car provided on moodle    
+
+    iconpath = 'https://apollo-media.codio.co.uk/media%2F1%2F9a9ed3e3e1ee96baf9da3e47d4570c5c-0e41f5b92e883928.webp'
+
+    # Add markers for every car on the map 
+    for car in results:
+        folium.Marker(
+            location=[car.latitude, car.longitude],
+            icon=folium.CustomIcon(iconpath, icon_size=(50, 30)),
+            popup=f"Vehicle ID: {car.vehicleId}<br>Place: {car.place}"
+        ).add_to(map)
+
+    # Save the map to an HTML file
+    map.save('templates/map.html')
+
+    # Render the template that displays the map
+    return render_template('map.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
