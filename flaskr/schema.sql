ac97e71b78bb1844badfbfc3f573de268c39abae
MELVIR SINGH <msingh09@student.bbk.ac.uk>
2023-06-22 17:36:48 +0000

    Create booking and car tables to prep for future features from Front-end

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -1,7 +1,8 @@
 DROP TABLE IF EXISTS user;
-DROP TABLE IF EXISTS post;
 DROP TABLE IF EXISTS card;
 DROP TABLE IF EXISTS feedback;
+DROP TABLE IF EXISTS booking;
+DROP TABLE IF EXISTS car;
 
 CREATE TABLE user (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -32,3 +33,35 @@ CREATE TABLE feedback (
   feedback TEXT NOT NULL,
   FOREIGN KEY (user_id) REFERENCES user (id)
 );
+
+CREATE TABLE booking (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  user_id INTEGER NOT NULL,
+  car_id INTEGER NOT NULL,
+  created TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  source TEXT NOT NULL,
+  destination TEXT NOT NULL,
+  status TEXT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES user (id)
+  FOREIGN KEY (car_id) REFERENCES car (id)
+);
+
+CREATE TABLE car (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  user_id INTEGER NOT NULL,
+  created TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  brand TEXT NOT NULL,
+  model TEXT NOT NULL,
+  colour TEXT NOT NULL,
+  next_service TEXT NOT NULL,
+  status TEXT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES user (id)
+);
+
+-- Preparing sample data before we develop the feature
+
+-- INSERT INTO car (user_id, brand, model, colour, next_service, status) VALUES (1, 'Mazda', '3 Hb', 'Red', '23/06/2023', 'Active');
+
+-- INSERT INTO car (user_id, brand, model, colour, next_service, status) VALUES (2, 'Mazda', '6', 'Blue', '23/12/2023', 'Active');
+
+-- INSERT INTO booking (user_id, car_id, source, destination, status) VALUES (3, 1, 'Wembley Park', 'Euston Square', 'Completed');
