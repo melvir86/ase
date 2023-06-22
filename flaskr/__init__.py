f9519a0bbfb0a17cc950ea42cd1ab179c92e4333
MELVIR SINGH <msingh09@student.bbk.ac.uk>
2023-06-22 17:51:15 +0000

    Started development of View Booking History for Customer

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -46,6 +46,9 @@ def create_app(test_config=None):
     from . import feedback
     app.register_blueprint(feedback.bp)
 
+    from . import booking
+    app.register_blueprint(booking.bp)
+
     # make url_for('index') == url_for('blog.index')
     # in another app, you might define a separate main index here with
     # app.route, while giving the blog blueprint a url_prefix, but for
