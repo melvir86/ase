9d6cc0af479adbdcfee9724fc8dc07e19ebe10c7
MELVIR SINGH <msingh09@student.bbk.ac.uk>
2023-06-28 22:13:45 +0000

    Converted card module into proper client / server frontend / backend architecture

--- flaskr/card.py
+++ flaskr/card.py
@@ -7,8 +7,12 @@ from flaskr.auth import login_required
 from flaskr.db import get_db
 
 import folium
+import requests
 
 bp = Blueprint('card', __name__)
+
+# CHANGE THE BELOW BASED ON YOUR OWN CODIO SUBDOMAIN FOR APPLICATION TO WORK CORRECTLY
+CODIO_SUBDOMAIN_ENDPOINT = 'https://platemessage-jargoncannon-8080.codio-box.uk/api'
 
 @bp.route('/')
 def index():
@@ -16,17 +20,23 @@ def index():
 
 @bp.route('/listCard')
 def listCard():
-    db = get_db()
-    cards = db.execute(
-        'SELECT *'
-        ' FROM card c JOIN user u ON c.user_id = u.id'
-        ' ORDER BY created DESC'
-    ).fetchall()
+    api_endpoint = CODIO_SUBDOMAIN_ENDPOINT + "/listCard"
+    cards = ""
+    #g.user['id']
+    params = {'uid': g.user['id']}
+
+    response = requests.post(api_endpoint, params=params)
+
+    if response.status_code == 200:
+        # Successful response
+        cards = response.json()
+
     return render_template('card/list.html', cards=cards)
 
 @bp.route('/createCard', methods=('GET', 'POST'))
 @login_required
 def createCard():
+    api_endpoint = CODIO_SUBDOMAIN_ENDPOINT + "/createCard"
     if request.method == 'POST':
         name = request.form['name']
         number = request.form['number']
@@ -37,46 +47,44 @@ def createCard():
         status = request.form['status']
         error = None
 
-        if not name:
-            error = 'Name is required.'
-        if not number:
-            error = 'Number is required.'
+        payload = {
+                "uid": g.user['id'],
+                "name": request.form['name'],
+                "number": request.form['number'],
+                "expiry_month": request.form['expiry_month'],
+                "expiry_year": request.form['expiry_year'],
+                "cve": request.form['cve'],
+                "description": request.form['description'],
+                "status": request.form['status'],
+        }
 
-        if error is not None:
-            flash(error)
-        else:
-            db = get_db()
-            db.execute(
-                'INSERT INTO card (user_id, name, number, expiry_month, expiry_year, cve, description, status)'
-                ' VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
-                (g.user['id'], name, number, expiry_month, expiry_year, cve, description, status)
-            )
-            db.commit()
+        response = requests.post(api_endpoint, json=payload)
+
+        if response.status_code == 201:
+            # Successful response
+            cards = response.json()
+
             return redirect(url_for('card.listCard'))
 
     return render_template('card/create.html')
 
 def get_card(id, check_author=True):
-    post = get_db().execute(
-        'SELECT *'
-        ' FROM card c JOIN user u ON c.user_id = u.id'
-        ' WHERE c.id = ?',
-        (id,)
-    ).fetchone()
+    api_endpoint = CODIO_SUBDOMAIN_ENDPOINT + "/" + str(id) + "/getCard"
+    card = ""
 
-    if post is None:
-        abort(404, f"Card id {id} doesn't exist.")
+    response = requests.post(api_endpoint)
+    if response.status_code == 200:
+        # Successful response
+        card = response.json()
 
-    if check_author and post['user_id'] != g.user['id']:
-        abort(403)
-
-    return post
+    return card
 
 @bp.route('/<int:id>/updateCard', methods=('GET', 'POST'))
 @login_required
 def updateCard(id):
     card = get_card(id)
 
+    api_endpoint = CODIO_SUBDOMAIN_ENDPOINT + "/" + str(id) + "/updateCard"
     if request.method == 'POST':
         name = request.form['name']
         number = request.form['number']
@@ -87,30 +95,35 @@ def updateCard(id):
         status = request.form['status']
         error = None
 
-        if not name:
-            error = 'Name is required.'
-        if not number:
-            error = 'Number is required.'
+        payload = {
+                "uid": g.user['id'],
+                "name": request.form['name'],
+                "number": request.form['number'],
+                "expiry_month": request.form['expiry_month'],
+                "expiry_year": request.form['expiry_year'],
+                "cve": request.form['cve'],
+                "description": request.form['description'],
+                "status": request.form['status'],
+        }
 
-        if error is not None:
-            flash(error)
-        else:
-            db = get_db()
-            db.execute(
-                'UPDATE card SET name = ?, number = ?, expiry_month = ?, expiry_year = ?, cve = ?, description = ?, status = ?'
-                ' WHERE id = ?',
-                (name, number, expiry_month, expiry_year, cve, description, status, id)
-            )
-            db.commit()
+        response = requests.post(api_endpoint, json=payload)
+
+        if response.status_code == 200:
+            # Successful response
+            cards = response.json()
+
             return redirect(url_for('card.listCard'))
 
-    return render_template('card/update.html', card=card)
+    return render_template('card/update.html', card=card[0])
 
 @bp.route('/<int:id>/deleteCard', methods=('POST',))
 @login_required
 def deleteCard(id):
-    get_card(id)
-    db = get_db()
-    db.execute('DELETE FROM card WHERE id = ?', (id,))
-    db.commit()
+    api_endpoint = CODIO_SUBDOMAIN_ENDPOINT + "/" + str(id) + "/deleteCard"
+
+    response = requests.post(api_endpoint)
+
+    if response.status_code == 200:
+        # Successful response
+        cards = response.json()
     return redirect(url_for('card.listCard'))
