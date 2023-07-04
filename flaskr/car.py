148431cea548b1c4343d4c0be111ad67999ee807
MELVIR SINGH <msingh09@student.bbk.ac.uk>
2023-07-04 19:44:48 +0000

    Converted car details into proper backend architecture

--- flaskr/car.py
+++ flaskr/car.py
@@ -6,13 +6,24 @@ from werkzeug.exceptions import abort
 from flaskr.auth import login_required
 from flaskr.db import get_db
 
+import requests
+
 bp = Blueprint('car', __name__)
+
+# CHANGE THE BELOW BASED ON YOUR OWN CODIO SUBDOMAIN FOR APPLICATION TO WORK CORRECTLY
+CODIO_SUBDOMAIN_ENDPOINT = 'https://platemessage-jargoncannon-8080.codio-box.uk/api'
 
 @bp.route('/listCarDetails')
 def listCarDetails():
-    db = get_db()
-    cars = db.execute(
-        'SELECT * FROM car WHERE user_id = ?',
-        (g.user['id'],)
-    ).fetchall()
+    api_endpoint = CODIO_SUBDOMAIN_ENDPOINT + "/listCarDetails"
+    cars = ""
+    #g.user['id']
+    params = {'uid': g.user['id']}
+
+    response = requests.post(api_endpoint, params=params)
+
+    if response.status_code == 200:
+        # Successful response
+        cars = response.json()
+
     return render_template('car/list.html', cars=cars)
